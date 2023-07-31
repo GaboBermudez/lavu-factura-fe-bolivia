b0f0822153b2be774a055d4ef9a29916e6a6e391
Gabriel Bermúdez <gbermudezmora@gmail.com>
2023-07-31 11:55:39 -0600

    apuntado a server en prod

--- src/services/FacturacionApi.js
+++ src/services/FacturacionApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const url = 'https://lavu-factura-api-bolivia-production.up.railway.app'
-const url = 'http://localhost:8080'
+const url = 'https://lavu-factura-api-bolivia-production.up.railway.app'
+// const url = 'http://localhost:8080'
 
 export async function enviarFactura(payload) {
   const result = await axios.post(`${url}/facturar`, payload)
