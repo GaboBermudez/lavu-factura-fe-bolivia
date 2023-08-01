8c067a4de091695c919514e3fb0aca5b79c1c76f
Gabriel Bermúdez <gbermudezmora@gmail.com>
2023-08-01 15:26:53 -0600

    pagos mixtos

--- src/components/FacturarForm.vue
+++ src/components/FacturarForm.vue
@@ -1,19 +1,36 @@
 <template>
   <div>
     <Toast />
-    <ConfirmDialog> 
+    <ConfirmDialog>
       <template #message>
         <div class="flex flex-column gap-5 p-5">
-          <h3 class="font-semibold">¿Está seguro que desea facturar la orden con los siguientes datos?</h3>
+          <h3 class="font-semibold">
+            ¿Está seguro que desea facturar la orden con los siguientes datos?
+          </h3>
           <ul>
-            <li>Orden de LAVU: <span class="font-semibold">{{ orderId }}</span></li>
-            <li>Método de pago: <span class="font-semibold">{{ metodoDePagoLabel }}</span></li>
-            <li v-if="pagoTarjeta">Número de tarjeta: <span class="font-semibold">{{ numeroTarjeta }}</span></li>
+            <li>
+              Orden de LAVU: <span class="font-semibold">{{ orderId }}</span>
+            </li>
+            <li>
+              Método de pago: <span class="font-semibold">{{ metodoDePagoLabel }}</span>
+            </li>
+            <li v-if="pagoTarjeta">
+              Número de tarjeta: <span class="font-semibold">{{ numeroTarjeta }}</span>
+            </li>
             <div v-if="esContribuyente">
-              <li>Documento de identidad: <span class="font-semibold">{{  documentodeIdentidadLabel }}</span></li>
-              <li>Nombre/Razón Social: <span class="font-semibold">{{ nombreRazonSocial }}</span></li>
-              <li>Número de identificación: <span class="font-semibold">{{ numeroDocumento }}</span></li>
-              <li>Email: <span class="font-semibold">{{ emailCliente }}</span></li>
+              <li>
+                Documento de identidad:
+                <span class="font-semibold">{{ documentodeIdentidadLabel }}</span>
+              </li>
+              <li>
+                Nombre/Razón Social: <span class="font-semibold">{{ nombreRazonSocial }}</span>
+              </li>
+              <li>
+                Número de identificación: <span class="font-semibold">{{ numeroDocumento }}</span>
+              </li>
+              <li>
+                Email: <span class="font-semibold">{{ emailCliente }}</span>
+              </li>
             </div>
           </ul>
         </div>
@@ -21,20 +38,20 @@
     </ConfirmDialog>
     <LoadingOverlay v-if="showLoadingSpinner" />
     <h1 class="mb-0 mt-5">Facturar</h1>
-    <Divider class="mt-0"/>
+    <Divider class="mt-0" />
     <div class="flex flex-column gap-4">
       <div class="flex flex-column gap-2">
         <label for="orderId">Orden de LAVU</label>
-        <InputText required id="orderId" type="text" v-model="orderId"/>
+        <InputText required id="orderId" type="text" v-model="orderId" />
       </div>
       <div class="flex flex-column gap-2">
         <label for="codigoMetodoPago">Método de pago</label>
-        <Dropdown 
-        id="codigoMetodoPago" 
-        v-model="codigoMetodoPago" 
-        :options="metodosPago" 
-        optionLabel="descripcion"
-        optionValue="codigo"
+        <Dropdown
+          id="codigoMetodoPago"
+          v-model="codigoMetodoPago"
+          :options="metodosPago"
+          optionLabel="descripcion"
+          optionValue="codigo"
         />
       </div>
       <Transition>
@@ -42,14 +59,26 @@
           <div class="flex justify-content-between">
             <div class="flex flex-column">
               <label for="primerosDigitosTarjeta">Primeros 4 dígitos de la tarjeta</label>
-              <InputNumber inputId="primerosDigitosTarjeta" type="text" v-model="primerosNumsTarjeta" :useGrouping="false" />
+              <InputNumber
+                inputId="primerosDigitosTarjeta"
+                type="text"
+                v-model="primerosNumsTarjeta"
+                :useGrouping="false"
+              />
             </div>
             <div class="flex flex-column">
               <label for="ultimosDigitosTarjeta">Últimos 4 dígitos de la tarjeta</label>
-              <InputNumber inputId="ultimosDigitosTarjeta" type="text" v-model="ultimosNumsTarjeta" :useGrouping="false" />
+              <InputNumber
+                inputId="ultimosDigitosTarjeta"
+                type="text"
+                v-model="ultimosNumsTarjeta"
+                :useGrouping="false"
+              />
             </div>
           </div>
-          <InlineMessage severity="info">Debe ingresar número de tarjeta si método de pago es TARJETA</InlineMessage>
+          <InlineMessage severity="info"
+            >Debe ingresar número de tarjeta si método de pago es TARJETA</InlineMessage
+          >
         </div>
       </Transition>
       <div class="card flex justify-content-center">
@@ -63,12 +92,12 @@
           <div class="flex flex-column gap-4">
             <div class="flex flex-column gap-2">
               <label for="codigoTipoDocumentoIdentidad">Documento de Identidad</label>
-              <Dropdown 
-              id="codigoTipoDocumentoIdentidad" 
-              v-model="codigoTipoDocumentoIdentidad" 
-              :options="documentosIdentidad" 
-              optionLabel="descripcion"
-              optionValue="codigo"
+              <Dropdown
+                id="codigoTipoDocumentoIdentidad"
+                v-model="codigoTipoDocumentoIdentidad"
+                :options="documentosIdentidad"
+                optionLabel="descripcion"
+                optionValue="codigo"
               />
             </div>
             <div class="flex flex-column gap-2">
@@ -93,7 +122,7 @@
 
 <script setup>
 import Button from 'primevue/button'
-import ConfirmDialog from 'primevue/confirmdialog' 
+import ConfirmDialog from 'primevue/confirmdialog'
 import Divider from 'primevue/divider'
 import Dropdown from 'primevue/dropdown'
 import InlineMessage from 'primevue/inlinemessage'
@@ -103,46 +132,61 @@ import LoadingOverlay from '@/components/LoadingOverlay.vue'
 import InputText from 'primevue/inputtext'
 import Toast from 'primevue/toast'
 import { computed, ref } from 'vue'
-import { useToast } from "primevue/usetoast"
-import { useConfirm } from "primevue/useconfirm"
+import { useToast } from 'primevue/usetoast'
+import { useConfirm } from 'primevue/useconfirm'
 import { enviarFactura } from '@/services/FacturacionApi'
 
 const documentosIdentidad = [
   {
     codigo: 1,
-    descripcion: 'CI - CEDULA DE IDENTIDAD',
+    descripcion: 'CI - CEDULA DE IDENTIDAD'
   },
   {
     codigo: 2,
-    descripcion: 'CEX - CEDULA DE IDENTIDAD DE EXTRANJERO',
+    descripcion: 'CEX - CEDULA DE IDENTIDAD DE EXTRANJERO'
   },
   {
     codigo: 3,
-    descripcion: 'PAS - PASAPORTE',
+    descripcion: 'PAS - PASAPORTE'
   },
   {
     codigo: 4,
-    descripcion: 'OD - OTRO DOCUMENTO DE IDENTIDAD',
+    descripcion: 'OD - OTRO DOCUMENTO DE IDENTIDAD'
   },
   {
     codigo: 5,
-    descripcion: 'NIT - NÚMERO DE IDENTIFICACIÓN TRIBUTARIA',
-  },
+    descripcion: 'NIT - NÚMERO DE IDENTIFICACIÓN TRIBUTARIA'
+  }
 ]
 
 const metodosPago = [
   {
     codigo: 1,
-    descripcion: 'EFECTIVO',
+    descripcion: 'EFECTIVO'
   },
   {
     codigo: 2,
-    descripcion: 'TARJETA',
+    descripcion: 'TARJETA'
   },
   {
     codigo: 7,
-    descripcion: 'TRANSFERENCIA BANCARIA',
+    descripcion: 'TRANSFERENCIA BANCARIA'
   },
+  {
+    codigo: 10,
+    descripcion: 'EFECTIVO - TARJETA',
+    isActive: 1
+  },
+  {
+    codigo: 13,
+    descripcion: 'EFECTIVO – TRANSFERENCIA BANCARIA',
+    isActive: 1
+  },
+  {
+    codigo: 18,
+    descripcion: 'TARJETA - TRANSFERENCIA BANCARIA',
+    isActive: 1
+  }
 ]
 
 // Form values
@@ -160,9 +204,11 @@ const numeroTarjeta = computed(() => {
   return Number(`${primerosNumsTarjeta.value}00000000${ultimosNumsTarjeta.value}`)
 })
 
-
 // Form manipulation
-const pagoTarjeta = computed(() => codigoMetodoPago.value === 2)
+const pagoTarjeta = computed(
+  () =>
+    codigoMetodoPago.value === 2 || codigoMetodoPago.value === 10 || codigoMetodoPago.value === 18
+)
 const pagoTarjetaValido = computed(() => {
   if (!pagoTarjeta.value) {
     return true
@@ -185,28 +231,37 @@ const enableFacturarBtn = computed(() => {
 const toast = useToast()
 
 const showSucessToast = () => {
-  toast.add({ severity: 'success', summary: 'Factura emitida exitosamente', detail: `Factura para la orden ${orderId.value} enviada con éxito`, life: 7000 })
+  toast.add({
+    severity: 'success',
+    summary: 'Factura emitida exitosamente',
+    detail: `Factura para la orden ${orderId.value} enviada con éxito`,
+    life: 7000
+  })
 }
 
 const showFailToast = () => {
   // const detail = response.data.errors[0].slice(0, 72)
-  toast.add({ severity: 'error', summary: `Error al facturar orden ${orderId.value}`, detail: `Hubo un problema al facturar la orden ${orderId.value}`, life: 7000 })
+  toast.add({
+    severity: 'error',
+    summary: `Error al facturar orden ${orderId.value}`,
+    detail: `Hubo un problema al facturar la orden ${orderId.value}`,
+    life: 7000
+  })
 }
-
 
 // Confirmar factura
 const confirm = useConfirm()
 const metodoDePagoLabel = computed(() => {
-    const { descripcion } = metodosPago.find(metodo => {
-      return metodo.codigo === codigoMetodoPago.value
-    })
-    return descripcion
+  const { descripcion } = metodosPago.find((metodo) => {
+    return metodo.codigo === codigoMetodoPago.value
+  })
+  return descripcion
 })
 const documentodeIdentidadLabel = computed(() => {
-    const { descripcion } = documentosIdentidad.find(documento => {
-      return documento.codigo === codigoTipoDocumentoIdentidad.value
-    })
-    return descripcion
+  const { descripcion } = documentosIdentidad.find((documento) => {
+    return documento.codigo === codigoTipoDocumentoIdentidad.value
+  })
+  return descripcion
 })
 
 const limpiarCampos = () => {
@@ -235,7 +290,6 @@ const confirmFactura = () => {
   })
 }
 
-
 // Facturacion
 const showLoadingSpinner = ref(false)
 
@@ -250,18 +304,17 @@ const facturar = async () => {
       numeroTarjeta: numeroTarjeta.value,
       nombreRazonSocial: nombreRazonSocial.value,
       numeroDocumento: numeroDocumento.value,
-      emailCliente: emailCliente.value,
+      emailCliente: emailCliente.value
     }
     await enviarFactura(payload)
     showSucessToast()
     limpiarCampos()
-  } catch(e) {
+  } catch (e) {
     showFailToast(e.response)
   } finally {
     showLoadingSpinner.value = false
   }
 }
-
 </script>
 
 <style>
